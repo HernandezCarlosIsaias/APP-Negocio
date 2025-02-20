8b61d3408683fcb20d8bd0185bdf74da4253926c
HernandezCarlosIsaias <120617047+HernandezCarlosIsaias@users.noreply.github.com.>
2025-02-19 22:37:28 -0300

    creamos las clases articulo, marca, categoria

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace dominio
 {
-    internal class Articulo
+    public class Articulo
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Stock { get; set; }
+        public string Caracteristica { get; set; }
+        public double Precio { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
     }
 }
--- dominio/Categoria.cs
+++ dominio/Categoria.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace dominio
 {
-    internal class Categoria
+    public class Categoria
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
     }
 }
